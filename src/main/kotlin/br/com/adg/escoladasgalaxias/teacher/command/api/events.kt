6e3539915e26d8daf1371cf774be68de51403a3d
Lucas Gertel <lgertel@avenuecode.com>
2022-07-02 00:56:17 -0300

    feat(command-model): created teacher aggregate, events and commands

--- src/main/kotlin/br/com/adg/escoladasgalaxias/teacher/command/api/events.kt
+++ src/main/kotlin/br/com/adg/escoladasgalaxias/teacher/command/api/events.kt
@@ -1,1 +1,42 @@
 package br.com.adg.escoladasgalaxias.teacher.command.api
+
+import br.com.adg.escoladasgalaxias.api.AuditEntry
+import java.io.Serializable
+
+abstract class AuditableAbstractEvent(open val auditEntry: AuditEntry) :
+	Serializable
+
+abstract class TeacherEvent(
+	open val aggregateIdentifier: TeacherId,
+	override val auditEntry: AuditEntry
+) : AuditableAbstractEvent(auditEntry)
+
+abstract class TeacherLessonEvent(
+	open val aggregateIdentifier: TeacherLessonId,
+	override val auditEntry: AuditEntry
+) : AuditableAbstractEvent(auditEntry)
+
+data class TeacherRegisteredEvent(
+	val name: String,
+	val email: String,
+	override val aggregateIdentifier: TeacherId,
+	override val auditEntry: AuditEntry
+) : TeacherEvent(aggregateIdentifier, auditEntry)
+
+data class TeacherLessonCreatedEvent(
+	open val aggregateIdentifier: TeacherLessonId,
+	override val auditEntry: AuditEntry
+): AuditableAbstractEvent(auditEntry)
+
+data class TeacherLessonAssignedEvent(
+	override val aggregateIdentifier: TeacherLessonId,
+	override val auditEntry: AuditEntry
+): TeacherLessonEvent(aggregateIdentifier, auditEntry)
+
+data class TeacherLessonPublishedEvent(
+	override val aggregateIdentifier: TeacherLessonId,
+	override val auditEntry: AuditEntry
+): TeacherLessonEvent(aggregateIdentifier, auditEntry)
+
+
+
--- src/main/kotlin/br/com/adg/escoladasgalaxias/teacher/command/api/valueObjects.kt
+++ src/main/kotlin/br/com/adg/escoladasgalaxias/teacher/command/api/valueObjects.kt
@@ -5,3 +5,9 @@ import java.util.UUID
 data class TeacherId(val identifier: UUID = UUID.randomUUID()) {
 	override fun toString(): String  = identifier.toString()
 }
+
+data class TeacherLessonId(val identifier: UUID = UUID.randomUUID()) {
+	override fun toString(): String  = identifier.toString()
+}
+
+const val ANONYMOUS = "anonymous"
